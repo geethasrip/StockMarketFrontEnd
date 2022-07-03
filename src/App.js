31a7db08388a6ebb98f1b7f92d10867771701c9d
geethap <geethasrilakshmi96@gmail.com>
2022-07-03 23:03:20 +0530

    Updated with delete and register functionalities

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+ import logo from './logo.svg';
 import './App.css';
 import CompanyListComponent from './components/CompanyListComponent';
-import AuthComponent from './components/Auth';
-import ReactDOM from "react-dom/client";
+import LoginComponent from './components/LoginComponent';
+
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
+import HeaderComponent from './components/HeaderComponent';
+import withNavigation from './components/withNavigation';
+import withParams from './components/params';
+import AuthenticatedRoute from './components/AuthenticatedRoute';
+import LogoutComponent from './components/LogoutComponent';
+import RegisterCompany from './components/RegisterCompany';
+import AddStock from './components/AddStockComponent';
 function App() {
+   const LoginComponentWithNavigation = withNavigation(LoginComponent);
+
+   	const CompanyListComponentWithParams = withParams(CompanyListComponent);
+
+     	const HeaderComponentWithNavigation = withNavigation(HeaderComponent);
+
+    	
   return (
     <div className="App">
-   {/* <CompanyListComponent></CompanyListComponent> */}
-
    <BrowserRouter>
+   <HeaderComponentWithNavigation/>
     <Routes>
-      <Route path="/" element={<AuthComponent/>}>
-        <Route path="/home" element={<CompanyListComponent />} />
-        
-      </Route>
+    <Route path="/" element={<LoginComponentWithNavigation />} />
+		<Route path="/login" element={<LoginComponentWithNavigation />} />
+    <Route path="/logout" element={<LogoutComponent/>} />
+    <Route path="/registerCompany" element={<AuthenticatedRoute><RegisterCompany/></AuthenticatedRoute>} />
+    <Route path="/addStock" element={<AuthenticatedRoute><AddStock/></AuthenticatedRoute>}/>
+        <Route path="/home" element={<AuthenticatedRoute><CompanyListComponentWithParams/></AuthenticatedRoute>} />
+
     </Routes>
     </BrowserRouter>
       
